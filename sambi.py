1621ba4911d187f63d8e6ff4f379c3f02d09b87b
Sambi Reddy Bommareddy(UST <206217@ust-global.com>
2023-05-22 16:13:58 +0530

    first commit

---
+++ sambi.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def main():
+
+ st.title("Shivaleela Streamlit app")
+
+ st.sidebar.title("Sidebar")
+
+ # Add content to the app
+
+ st.write("Welcome to Shivaleela app!")
+
+if __name__ == "__main__":
+
+   main()
