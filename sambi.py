ae18b63f926b72179da5ffb30d522b4180c55dcb
Sambi8998 <123440623+Sambi8998@users.noreply.github.com>
2023-05-22 15:27:48 +0530

    Add files via upload

---
+++ sambi.py
@@ -0,0 +1,8 @@
+import streamlit as st
+def main():
+ st.title(" Sambi Streamlit app")
+ st.sidebar.title("Sidebar")
+ # Add content to the app
+ st.write("Welcome to Sambi app!")
+if __name__ == "__main__":
+ main()
